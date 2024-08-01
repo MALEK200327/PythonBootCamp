58416d14beffb377995159e3689e6c6c1aac0460
malek <mmmahmoud1@sheffield.ac.uk>
2024-08-02 02:14:17 +0300

    adding p7

--- RockPaperS.py
+++ RockPaperS.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -26,3 +28,24 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+option=[rock,paper,scissors]
+print(option[user_choice])
+computer_choice = random.randint(0,2)
+print(option[computer_choice])
+if user_choice == computer_choice:
+    print("It's a draw!")
+elif user_choice == 0 and computer_choice == 1:
+    print("Paper covers rock, you lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("Rock crushes scissors, you win!")
+elif user_choice == 1 and computer_choice == 0:
+    print("Paper covers rock, you win!")
+elif user_choice == 1 and computer_choice == 2:
+    print("Scissors cuts paper, you lose!")
+elif user_choice == 2 and computer_choice == 0:
+    print("Rock crushes scissors, you lose!")
+elif user_choice == 2 and computer_choice == 1:
+    print("Scissors cuts paper, you win!")
+else:
+    print("Invalid input")  # This line is unreachable, but I'll leave it here for
