39d17ae5707b9d02d3f1b570a6a52d972445768f
malek <mmmahmoud1@sheffield.ac.uk>
2024-08-08 18:00:23 +0300

    calculator

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,51 @@
-def addition(a,b):
+
+def addition(a, b):
     return a + b
 
-def subtraction(a,b):
+def subtraction(a, b):
     return a - b
 
-def multiplaction(a,b):
+def multiplication(a, b):
     return a * b
 
-def division(a,b):
-    return a/b
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
 
 operations = {
-    "+":addition,
-    "-":subtraction,
-    "*":multiplaction,
-    "/":division
+    "+": addition,
+    "-": subtraction,
+    "*": multiplication,
+    "/": division
 }
 
-print(operations["+"](4,5))
+def calculator():
+    close_calculator = False
+    num1 = float(input("Enter the first number: \n"))
+    while not close_calculator:
+        for symbol in operations:
+            print(symbol)
+        operation = input("Enter operation: \n")
+        num2 = float(input("Enter the second number: \n"))
+        if operation in operations:
+            answer = operations[operation](num1, num2)
+            print(f"{num1} {operation} {num2} = {answer}")
+            reset_calculator = input("Enter 'Y' to continue with the previous number or 'N' to start a new calculation: ").upper()
+            if reset_calculator == "Y":
+                num1 = answer
+            elif reset_calculator == "N":
+                close_calculator = True
+                print("\n" * 20)  # Clear the console
+                calculator()
+            else:
+                print("Invalid input. Exiting calculator.")
+                close_calculator = True
+        else:
+            print("Invalid operation. Please select a valid operation.")
+
+calculator()
+
+
+
